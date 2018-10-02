9f3187fe2825da89e92849c5bc031298729f1dcd
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-02 15:36:57 -0700

    is sort is ascending only

--- ex05/ft_is_sort.c
+++ ex05/ft_is_sort.c
@@ -6,24 +6,20 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/02 02:04:51 by seli              #+#    #+#             */
-/*   Updated: 2018/10/02 02:17:13 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/02 15:36:40 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int	i;
-	int prev;
-	int result;
 
 	i = 0;
 	prev = 0;
 	while (i < length - 1)
 	{
-		result = f(tab[i], tab[i + 1]);
-		if ((prev < 0 && result > 0) || (prev > 0 && result < 0))
+		if (f(tab[i], tab[i + 1]) > 0)
 			return (0);
-		prev = (prev > 0 || result > 0) ? 1 : -1;
 		i++;
 	}
 	return (1);
