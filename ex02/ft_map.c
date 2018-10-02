5fd7d71f7b42fc889d62c5a0008a0f1b488db84c
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-02 00:02:26 -0700

    ex01-ex02

--- ex02/ft_map.c
+++ ex02/ft_map.c
@@ -1,20 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/01 23:17:21 by seli              #+#    #+#             */
-/*   Updated: 2018/10/02 00:01:20 by seli             ###   ########.fr       */
+/*   Created: 2018/10/01 23:23:10 by seli              #+#    #+#             */
+/*   Updated: 2018/10/02 00:01:17 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_foreach(int *tab, int length, void (*f)(int))
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
 {
+	int *applied;
 	int i;
 
 	i = 0;
+	applied = malloc(sizeof(int) * length);
 	while (i < length)
-		f(tab[i++]);
+	{
+		applied[i] = f(tab[i]);
+		i++;
+	}
+	return (applied);
 }
