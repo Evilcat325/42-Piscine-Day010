590feea3ceb15b9d886f2a768c3538b53660a9d9
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-02 16:03:37 -0700

    ex08

---
+++ ex08/ft_advanced_sort_wordtab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_advanced_sort_wordtab.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/02 16:02:16 by seli              #+#    #+#             */
+/*   Updated: 2018/10/02 16:03:26 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_advanced_sort_wordtab(char **tab, int (*cmp)(char *, char *))
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	if (!tab[0])
+		return ;
+	i = 0;
+	while (tab[i + 1])
+	{
+		j = 0;
+		while (tab[j + 1])
+		{
+			if (cmp(tab[j], tab[j + 1]) > 0)
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
